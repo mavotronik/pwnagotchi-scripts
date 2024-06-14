3b4e3c2146c3ff67358bb67176d9f7ae529d2c8c
Matvey <66329687+mavotronik@users.noreply.github.com>
2024-06-14 18:55:28 +0300

    Update bt-reconnect.sh

--- bt-reconnect.sh
+++ bt-reconnect.sh
@@ -1,10 +1,8 @@
-bluetoothctl -- remove <your mac>
+sudo bluetoothctl -- remove <your mac>
 sleep 2
-bluetoothctl -- scan on
-sleep 5
-bluetoothctl -- pair <your mac>
-sleep 10
-bluetoothctl -- scan off
+sudo timeout -s 2 10 sudo bluetoothctl -- scan on
+sleep 1
+sudo bluetoothctl -- pair <your mac>
 
 exit 0
 
